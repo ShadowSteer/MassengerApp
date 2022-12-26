b51a3948bef8f632e890074b970f242337fd9806
bilal maymash <black-70055@hotmail.com>
2022-12-26 16:16:20 +0300

    Getting Started

---
+++ MassengerApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MassengerApp
+//
+//  Created by Bilal Maymash on 2022-12-26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
